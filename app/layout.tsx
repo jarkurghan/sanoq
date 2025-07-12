ed1f27fbb0d1fab49df24a003e7974dfd235c6df
jarkurghan <jarkurghan@gmail.com>
2025-07-12 16:08:54 +0500

    fixed issue

---
+++ app/layout.tsx
@@ -0,0 +1,23 @@
+import type React from "react";
+import { Inter } from "next/font/google";
+import { ThemeProvider } from "@/contexts/theme-context";
+import "@/styles/globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+type Props = {
+    children: React.ReactNode;
+};
+
+export default function Layout({ children }: Props) {
+    return (
+        <html suppressHydrationWarning>
+            <head>
+                <link rel="icon" href="/favicon.ico" sizes="any" />
+            </head>
+            <body className={inter.className}>
+                <ThemeProvider>{children}</ThemeProvider>
+            </body>
+        </html>
+    );
+}
