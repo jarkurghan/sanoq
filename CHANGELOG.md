d765d88943874e6f9918453a095c264d588f3796
jarkurghan <jarkurghan@gmail.com>
2025-07-26 17:46:56 +0500

    new design

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,32 +2,53 @@
 
 Barcha versiya o‘zgarishlari shu yerda qayd etiladi.
 
+### v1.2.0 — date
+
+**O‘zgartirildi:**   
+- Design butunlay qayta qurildi
+    - Sidebar Left olib tashlandi
+    - Komponentlar reuseble
+        - Container
+    - CSS maqola janriga yaqinroq
+        - font size kattaroq
+        - content o'rtada
+    - O'zgarmas umumiy CSS stylelar
+        - div
+        - p
+        - span
+        - a
+
+**Tuzatildi:**  
+- Konvertordagi kasr sonni chiqarishdagi xato tuzatildi
+
+---
+
 ### v1.1.1 — 2025-07-13
 
 **Tuzatildi:**  
-• Sahifalar eni responsive qilindi;
-• Kalkulyatorda hisob kitob yaxshilandi;
-• Rus tilidagi xatolar tuzatildi;
+- Sahifalar eni responsive qilindi
+- Kalkulyatorda hisob kitob yaxshilandi
+- Rus tilidagi xatolar tuzatildi
 
 ---
 
 ### v1.1.0 — 2025-07-12
 
 **Yangi:**  
-• Converter uchun yechim qo‘shildi.
-• Google natijalarida favicon chiqishi uchun `<link rel="icon">` tegi `<head>` ga qo‘shildi.
+- Converter uchun yechim qo‘shildi.
+- Google natijalarida favicon chiqishi uchun `<link rel="icon">` tegi `<head>` ga qo‘shildi.
 
 **O‘zgartirildi:**  
-• Converterda RightValue olib tashlandi. Endi faqat chap tarafga son kiritiladi, o‘ng tarafda natija avtomatik ko‘rsatiladi.
-• Navbardagi logo almashtirildi.
+- Converterda RightValue olib tashlandi. Endi faqat chap tarafga son kiritiladi, o‘ng tarafda natija avtomatik ko‘rsatiladi.
+- Navbardagi logo almashtirildi.
 
 **Tuzatildi:**  
-• Converter logikasi yaxshilandi va aniqlashtirildi.
-• Maqolalar sahifasiga header va chap sidebar qo'shildi
+- Converter logikasi yaxshilandi va aniqlashtirildi.
+- Maqolalar sahifasiga header va chap sidebar qo'shildi
 
 ---
 
 ### v1.0.0 — 2025-06-29
 
 **Yangi:**  
-• Dastlabki versiya chiqarildi.
+✓ Dastlabki versiya chiqarildi.
--- app/[lang]/about/page.tsx
+++ app/[lang]/about/page.tsx
@@ -4,6 +4,7 @@ import { getTranslation } from "@/lib/translater/i18n";
 import { Language } from "@/types/language";
 import type { Metadata } from "next";
 import { use } from "react";
+import Container from "@/components/common/container";
 
 type Props = {
     params: Promise<{ lang: Language }>;
@@ -79,49 +80,44 @@ export default function AboutPage({ params }: Props) {
 
     return (
         <div className="flex">
-            <div className="flex-1 container py-4 px-4 sm:px-8 max-w-6xl ml-0 lg:ml-64 text-justify">
-                <div className="mb-8">
+            <Container className="flex-1 prose prose-lg prose-sm text-justify">
+                {/* <div className="flex-1 container py-4 px-4 sm:px-8 max-w-6xl ml-0 lg:ml-64 text-justify"> */}
+                <div>
                     <header>
-                        <h1 className="text-2xl font-bold mb-4">{t("about.title")}</h1>
-                        <p className="text-sm mb-4">{t("about.description")}</p>
+                        <h1>{t("about.title")}</h1>
+                        <p>{t("about.description")}</p>
                     </header>
-                    <div className="grid grid-cols-[1fr_auto] gap-x-2 mb-4">
-                        <div className="text-lg font-medium">•</div>
-                        <h3 className="text-lg font-medium">{t("nav.conversion")}</h3>
+                    <div>
+                        <h3>{t("nav.conversion")}</h3>
                         <div></div>
-                        <p className="text-sm">{t("about.page.conversion.desription")}</p>
+                        <p>{t("about.page.conversion.desription")}</p>
                     </div>
-                    <div className="grid grid-cols-[1fr_auto] gap-x-2 mb-4">
-                        <div className="text-lg font-medium">•</div>
-                        <h3 className="text-lg font-medium">{t("nav.calculator")}</h3>
+                    <div>
+                        <h3>{t("nav.calculator")}</h3>
                         <div></div>
-                        <p className="text-sm">{t("about.page.calculator.desription")}</p>
+                        <p>{t("about.page.calculator.desription")}</p>
                     </div>
-                    <div className="grid grid-cols-[1fr_auto] gap-x-2 mb-4">
-                        <div className="text-lg font-medium">•</div>
-                        <h3 className="text-lg font-medium">{t("nav.publications")}</h3>
+                    <div>
+                        <h3>{t("nav.publications")}</h3>
                         <div></div>
-                        <p className="text-sm">{t("about.page.publications.desription")}</p>
+                        <p>{t("about.page.publications.desription")}</p>
                     </div>
-                    {/* <div className="grid grid-cols-[1fr_auto] gap-x-2 mb-4">
-                        <div className="text-lg font-medium">•</div>
-                        <h3 className="text-lg font-medium">{t("nav.code")}</h3>
+                    {/* <div>
+                        <h3>{t("nav.code")}</h3>
                         <div></div>
-                        <p className="text-sm">{t("about.page.code.desription")}</p>
+                        <p>{t("about.page.code.desription")}</p>
                     </div> */}
                 </div>
 
-                <div className="grid gap-8">
+                <div>
                     <div>
-                        <h2 className="text-2xl font-bold mb-4">{t("about.share.title")}</h2>
-                        <div className="sm:grid grid-cols-[1fr_auto] gap-x-4">
-                            <p className="text-sm">{t("about.share.description")}</p>
-                            <div className="mt-4 text-card-foreground w-full sm:w-40 py-4 flex items-center justify-center flex-col gap-2">
+                        <h2>{t("about.share.title")}</h2>
+                        <div>
+                            <p>{t("about.share.description")}</p>
+                            <div>
                                 <Share2 size={30} />
                                 <ShareAppURL lang={lang}>
-                                    <div className="px-6 py-1 text-white text-base font-medium bg-blue-500 border-none rounded-lg cursor-pointer transition-colors text-center no-underline inline-flex items-center justify-center gap-2">
-                                        {t("about.share.button")}
-                                    </div>
+                                    <div>{t("about.share.button")}</div>
                                 </ShareAppURL>
                             </div>
                         </div>
@@ -129,28 +125,24 @@ export default function AboutPage({ params }: Props) {
 
                     {/* to-do: donat */}
 
-                    <div className="grid gap-8">
+                    <div>
                         <div>
-                            <h2 className="text-2xl font-bold mb-4">{t("about.contact.title")}</h2>
-                            <div className="sm:grid grid-cols-[1fr_auto] gap-x-4">
-                                <div className="space-y-2 text-sm mb-4">
+                            <h2>{t("about.contact.title")}</h2>
+                            <div>
+                                <div>
                                     <ul>
-                                        <li className="mb-1">• {t("about.contact.description1")}</li>
-                                        <li className="mb-1">• {t("about.contact.description2")}</li>
-                                        <li className="mb-1">• {t("about.contact.description3")}</li>
-                                        <li className="mb-1">• {t("about.contact.description4")}</li>
-                                        <li className="mb-1">• {t("about.contact.description5")}</li>
-                                        <li className="mb-1">• {t("about.contact.description6")}</li>
+                                        <li>{t("about.contact.description1")}</li>
+                                        <li>{t("about.contact.description2")}</li>
+                                        <li>{t("about.contact.description3")}</li>
+                                        <li>{t("about.contact.description4")}</li>
+                                        <li>{t("about.contact.description5")}</li>
+                                        <li>{t("about.contact.description6")}</li>
                                     </ul>
                                     <strong>{t("about.contact.description.bold")}</strong> {t("about.contact.description.def")}
                                 </div>
-                                <div className="mt-4 text-card-foreground w-full sm:w-40 py-4 flex items-center justify-center flex-col gap-2">
+                                <div>
                                     <Send size={30} />
-                                    <a
-                                        href="https://t.me/full_full_stack"
-                                        target="_blank"
-                                        className="px-6 py-1 text-white text-base font-medium bg-blue-500 border-none rounded-lg cursor-pointer transition-colors text-center no-underline inline-flex items-center justify-center gap-2"
-                                    >
+                                    <a href="https://t.me/full_full_stack" target="_blank">
                                         {t("about.contact.button")}
                                     </a>
                                 </div>
@@ -158,7 +150,7 @@ export default function AboutPage({ params }: Props) {
                         </div>
                     </div>
                 </div>
-            </div>
+            </Container>
         </div>
     );
 }
